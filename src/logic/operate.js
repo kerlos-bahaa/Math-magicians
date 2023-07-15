e647fd0a596756840849f8fb58389c6799e26e6d
kerlosbahaa3 <aboelking4@gmail.com>
2023-07-15 18:47:11 +0300

    fix

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,28 +3,26 @@ import Big from 'big.js';
 export default function operate(numberOne, numberTwo, operation) {
   const one = Big(numberOne);
   const two = Big(numberTwo);
-  if (operation === '+') {
-    return one.plus(two).toString();
+  switch (operation) {
+    case '+':
+      return one.plus(two).toString();
+    case '-':
+      return one.minus(two).toString();
+    case 'x':
+      return one.times(two).toString();
+    case '÷':
+      try {
+        return one.div(two).toString();
+      } catch (err) {
+        return "Can't divide by 0.";
+      }
+    case '%':
+      try {
+        return one.mod(two).toString();
+      } catch (err) {
+        return "Can't find modulo as can't divide by 0.";
+      }
+    default:
+      throw Error(`Unknown operation '${operation}'`);
   }
-  if (operation === '-') {
-    return one.minus(two).toString();
-  }
-  if (operation === 'x') {
-    return one.times(two).toString();
-  }
-  if (operation === '÷') {
-    try {
-      return one.div(two).toString();
-    } catch (err) {
-      return "Can't divide by 0.";
-    }
-  }
-  if (operation === '%') {
-    try {
-      return one.mod(two).toString();
-    } catch (err) {
-      return "Can't find modulo as can't divide by 0.";
-    }
-  }
-  throw Error(`Unknown operation '${operation}'`);
 }
